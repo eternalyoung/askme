a79bfbdbba43321976b3857aee2c87a0c63c6e23
andrew <kondratev-28@mail.ru>
2022-12-10 22:12:47 +0700

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'questions#index'
   
   resources :questions do
-    put '/questions/:id/hide'
-    put '/questions/:id/unhide'
+    put '/questions/:id/hide', to: 'questions#hide'
+    put '/questions/:id/unhide', to: 'questions#unhide'
   end
 end
