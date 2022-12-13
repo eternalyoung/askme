7a0630f4560341a05b2df9dc5a2cde227c973121
andrew <kondratev-28@mail.ru>
2022-12-14 01:58:50 +0700

    routes style fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
 
-    resources :users, only: %i[new create destroy]
-    resource :session
-    resources :questions do
-      member do
+  resources :users, only: %i[new create destroy]
+  resource :session
+  resources :questions do
+    member do
       put :hide
       put :unhide
     end
