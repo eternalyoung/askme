948b6902be6bdf2fdec9d434d8de6382727b9178
andrew <kondratev-28@mail.ru>
2022-12-16 22:12:12 +0700

    asker v.0.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_16_070958) do
+ActiveRecord::Schema.define(version: 2022_12_16_144718) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_12_16_070958) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "hidden", default: false, null: false
     t.string "answer"
+    t.integer "asker_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
