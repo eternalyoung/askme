842a8d1a5e2649ff85ef5575b224f9582fe16e47
andrew <kondratev-28@mail.ru>
2022-12-18 14:30:46 +0700

    validates for user's header color

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   before_validation :downcase_nickname
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w_+.\-]+@[\w\-]+(\.[\w\-]+)*\.[a-zA-Z]+\Z/ }
-  validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A[\da-z_]*\Z/ }  
+  validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A[\da-z_]*\Z/ }
+  validates :header_color, format: { with: /\A#[a-z0-9]{6}\Z/ }
 
   has_many :questions, dependent: :delete_all
 
