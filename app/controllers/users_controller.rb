84aa6c48f36a4f8a345619f28ef23ae24cb5d702
andrew <kondratev-28@mail.ru>
2022-12-21 11:31:52 +0700

    404 when user not found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
   end
 
   def set_user
-    @user = User.find_by(nickname: params[:nickname])
+    @user = User.find_by!(nickname: params[:nickname])
   end
 
   def authorize_user
