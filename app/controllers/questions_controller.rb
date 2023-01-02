239d797e93b5845bde1f39e1a672172cf15a06f0
andrew <kondratev-28@mail.ru>
2023-01-02 22:54:41 +0700

    last to limit fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -33,9 +33,9 @@ class QuestionsController < ApplicationController
 
   def index
     @question = Question.new
-    @questions = Question.order(created_at: :desc).last(10)
-    @tags = Tag.order(created_at: :desc).last(50)
-    @users = User.order(created_at: :desc).last(10)
+    @questions = Question.order(created_at: :desc).limit(10)
+    @tags = Tag.order(created_at: :desc).limit(50)
+    @users = User.order(created_at: :desc).limit(10)
   end
 
   def new
